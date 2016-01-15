2ca5cb7713a18a93c0e5364c65cccc1f67e439a6
Nivanka Fonseka <nivanka@silverstripers.com>
2016-01-15 09:48:00 +0530

    added link post to the site

---
+++ code/Control/LinkPost.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by Nivanka Fonseka (nivanka@silverstripers.com).
+ * User: nivankafonseka
+ * Date: 1/15/16
+ * Time: 8:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class LinkPost extends NewsPost
+{
+
+	private static $db = array(
+		'ShareLink'			=> 'Varchar(500)',
+		'LinkTarget'		=> 'Enum("_self,_blank")'
+	);
+
+	public function getCMSFields()
+	{
+		$fields = parent::getCMSFields();
+
+		$fields->removeByName(array(
+			'Content',
+			'URLSegment'
+		));
+
+		$fields->addFieldsToTab('Root.Main', array(
+			TextField::create('ShareLink', 'Link'),
+			DropdownField::create('LinkTarget', 'Target')
+				->setSource(array(
+					'_self'		=> 'Self',
+					'_blank'	=> 'Open in a new window'
+				))
+		), 'Summary');
+
+
+		return $fields;
+	}
+
+	public function Link($action = null) {
+		return $this->ShareLink;
+	}
+
+
+}
+
+class LinkPost_Controller extends NewsPost_Controller
+{
+
+	public function init(){
+		parent::init();
+		return $this->redirect($this->ShareLink);
+	}
+
+}
